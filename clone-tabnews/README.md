4f9c49db639af41fb23d5b461742385385c8461d
Renato Oliveira <renatoj.oliveiracruz@gmail.com>
2026-06-21 06:59:30 +0000

    feat: scripts ´lint:check´ e ´lint:fix´

--- clone-tabnews/README.md
+++ clone-tabnews/README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do TabNews.com.br para o curso.dev que estou acompanhando.
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home() { 
-    return <h1>Kailany Mikaela Oliveira da Cruz</h1>
+function Home() {
+  return <h1>Kailany Mikaela Oliveira da Cruz</h1>;
 }
 
 export default Home;
-
